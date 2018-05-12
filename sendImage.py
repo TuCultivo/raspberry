c7d4a2736d8af06f9c7fb536aee247db2c668fbe
mhinca20 <mhinca20@eafit.edu.co>
2018-05-12 14:01:24 -0500

    Reenviando post cuando el response no sea bueno

--- sendImage.py
+++ sendImage.py
@@ -1,4 +1,9 @@
 import requests,json
 url = 'http://pi2tucultivo.dis.eafit.edu.co/'
 files ={'file':open('1_1_1_2.jpg', 'rb')}
-r=requests.post(url, files=files)
+enviado=False
+while enviado==False:
+    r=requests.post(url, files=files)
+    if r.status_code == 200:
+        enviado=True
+        print(r.status_code)
